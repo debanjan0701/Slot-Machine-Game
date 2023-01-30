47a1e57c7878b14bcbe678b05e9612cd4bbcd9f4
Debanjan Ghosh <93243921+debanjan0701@users.noreply.github.com>
2023-01-30 14:24:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import SlotM from "./SlotM";
+
+
+const App = () => {
+    return (
+        <>
+            <h1 className="heading_style">
+                🎰 Welcome to <span style={{ fontWeight: 'bold' }}>Slot Machine Game🎰</span>
+            </h1>
+            <div className="slotmachine">
+                <SlotM x = '😄' y = '😄' z = '😄'/>
+                <SlotM x = '😄' y = '😄' z = '👍'/>
+                <SlotM x = '😄' y = '👊' z = '😄'/>
+                <SlotM x = '😄' y = '😄' z = '😄'/>
+            </div>
+
+        </>
+    );
+}
+
+export default App;
